956e16445313be5293d3c7ee38c4c8af327dd7b6
EhtQuyet <ntq.145@gmail.com>
2021-02-28 11:54:47 +0700

    fix config

--- server/config/config.js
+++ server/config/config.js
@@ -1,21 +1,21 @@
-
 const config = {
 
   development: {
     secret: 'AIBOLIT',
-    MONGO_URI: 'mongodb+srv://hbien1999:1@cluster0.rhmeb.mongodb.net/NCKH?retryWrites=true&w=majority',
-    port: 27017,
-    "cos": {
-      "credentials": {
-        "serviceInstanceId": "crn:v1:bluemix:public:cloud-object-storage:global:a/c8892a299ea04a629bf4ed7f5ed8a5b1:cbf00124-96fd-48d5-be8f-f195a70420b2::",
-        "endpoint": "https://s3.ap.cloud-object-storage.appdomain.cloud",
-        "apiKeyId": "rki429NHwsuGE2zwKLWdgDUoJvK7EJtrpdrXCG0-wPSV",
-        "ibmAuthEndpoint": "https://iam.ng.bluemix.net/oidc/token",
-      },
-      "bucketName": "aibolit-collect"
-    },
 
-  }
+    // MONGO_URI: 'mongodb+srv://hbien1999:1@cluster0.rhmeb.mongodb.net/NCKH?retryWrites=true&w=majority',
+    MONGO_URI: 'mongodb://hbien1999:1@cluster0-shard-00-00.rhmeb.mongodb.net:27017,cluster0-shard-00-01.rhmeb.mongodb.net:27017,cluster0-shard-00-02.rhmeb.mongodb.net:27017/NCKH?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+    port: 27017,
+    'cos': {
+      'credentials': {
+        'serviceInstanceId': 'crn:v1:bluemix:public:cloud-object-storage:global:a/c8892a299ea04a629bf4ed7f5ed8a5b1:cbf00124-96fd-48d5-be8f-f195a70420b2::',
+        'endpoint': 'https://s3.ap.cloud-object-storage.appdomain.cloud',
+        'apiKeyId': 'rki429NHwsuGE2zwKLWdgDUoJvK7EJtrpdrXCG0-wPSV',
+        'ibmAuthEndpoint': 'https://iam.ng.bluemix.net/oidc/token',
+      },
+      'bucketName': 'aibolit-collect',
+    },
+  },
 };
 
 
--- server/config/db.js
+++ server/config/db.js
@@ -3,4 +3,4 @@ import { getConfig } from './config';
 
 const config = getConfig(process.env.NODE_ENV);
 mongoose.Promise = global.Promise;
-export const connect = () => mongoose.connect(config.MONGO_URI);
+export const connect = () => mongoose.connect(config.MONGO_URI, { useNewUrlParser: true });
