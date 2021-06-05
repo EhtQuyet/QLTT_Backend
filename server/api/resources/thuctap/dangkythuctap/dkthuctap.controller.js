97bdd23371625e733db774e087e32167ff4c4b37
HaiBien <lvh.bien@gmail.com>
2021-06-05 11:36:09 +0700

    Cập nhật đăng ký thực tập

--- server/api/resources/thuctap/dangkythuctap/dkthuctap.controller.js
+++ server/api/resources/thuctap/dangkythuctap/dkthuctap.controller.js
@@ -11,6 +11,7 @@ export async function getAll(req, res) {
   try {
     const query = queryHelper.extractQueryParam(req);
     const { criteria, options } = query;
+    console.log('query', query);
     options.sort = { created_at: -1 }
     options.populate = [
       { path: 'sinh_vien', select: 'ten_sinh_vien ma_sinh_vien' },
