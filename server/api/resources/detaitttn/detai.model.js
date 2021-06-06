151d61dae0247ac34200fb7a4790c768c6ca8e19
HaiBien <lvh.bien@gmail.com>
2021-06-06 07:51:55 +0700

    aaaaaaaaaaaaaaa

--- server/api/resources/detaitttn/detai.model.js
+++ server/api/resources/detaitttn/detai.model.js
@@ -37,7 +37,7 @@ const schema = new Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: TUKHOA,
   }],
-
+  file: {type: String},
   isActive: { type: Boolean, default: true },
   is_deleted: { type: Boolean, default: false, select: false },
 }, {
